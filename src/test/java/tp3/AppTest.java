a80ecd802e59efaa465785a1d8ec9b087d69567d
Marouen-Mahou <mahou.marouen@gmail.com>
2021-12-18 13:05:13 +0100

    Paramtrized Test

--- src/test/java/tp3/AppTest.java
+++ src/test/java/tp3/AppTest.java
@@ -1,10 +1,12 @@
 package tp3;
 
-import org.junit.AfterClass;
-import static org.junit.Assert.assertEquals;
-import org.junit.BeforeClass;
-
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -15,9 +17,13 @@ public class AppTest {
     
     public static WebDriver driver;
     
-    @BeforeClass
+    @BeforeAll
     public static void setup() {
         System.setProperty("webdriver.chrome.driver","F:\\chromedriver.exe");
+    }
+    
+    @BeforeEach
+    public static void prepare(){
         driver = new ChromeDriver();
     }
     
@@ -28,6 +34,30 @@ public class AppTest {
         
         //Select Language
         selectLanguage("Backbone.js");
+        
+        //Add todos
+        addTodo("Meet a Friend");
+        addTodo("Buy meat");
+        addTodo("Wash car");
+        
+        //Check input
+        driver.findElement(By.cssSelector("body > section > section > ul > li:nth-child(1) > div > input")).click();
+        
+        //Get Item number
+        WebElement count = driver.findElement(By.cssSelector("body > section > footer > span > strong"));
+        
+        //Test
+        assertEquals("2",count.getText());
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"Backbone.js","Dojo","Vue.js"})
+    public void testTodoWebSite(String language) {
+        //Open URL
+        driver.get("https://www.todomvc.com/");
+        
+        //Select Language
+        selectLanguage(language);
         
         //Add todos
         addTodo("Meet a Friend");
@@ -55,8 +85,8 @@ public class AppTest {
         input.clear();
     }
     
-    @AfterClass
+    @AfterEach
     public static void teardown() {
-        driver.close();
+        driver.quit();
     }
 }
